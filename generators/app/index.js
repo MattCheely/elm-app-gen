b0de9e1e36193fd0468d34f4907c966e603097b9
Matthew Cheely <matthew.cheely@genesys.com>
2019-01-02 19:38:36 -0500

    Turn some lets into consts

--- generators/app/index.js
+++ generators/app/index.js
@@ -51,7 +51,7 @@ module.exports = class extends Generator {
   }
 
   async install() {
-    let installer = this.answers.installer;
+    const installer = this.answers.installer;
 
     this.installDependencies({
       bower: false,
