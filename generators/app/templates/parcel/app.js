271d0e11b6e5b7fd44ffcb9f71a6918efbd7a938
Dillon Kearns <dillon@dillonkearns.com>
2019-07-02 09:54:44 +0200

    Add conditional to prevent wiring in elm-debug-transformer in prod.

--- generators/app/templates/parcel/app.js
+++ generators/app/templates/parcel/app.js
@@ -1,6 +1,11 @@
 import { Elm } from "../Main.elm";
 import * as ElmDebugger from "elm-debug-transformer";
-ElmDebugger.register();
+
+if (process.env.NODE_ENV === 'development') {
+  // Only runs in development and will be stripped from production build.
+  // See https://parceljs.org/production.html
+  ElmDebugger.register();
+}
 
 Elm.Main.init({
   node: document.getElementById("app")
