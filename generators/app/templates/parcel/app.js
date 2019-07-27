540a2e8ebe669005e43ea8d763f378ea71f03594
Matthew Cheely <matt.cheely@gmail.com>
2019-07-26 21:44:52 -0400

    Don't include elm-debug-transformer outside of develop mode.

--- generators/app/templates/parcel/app.js
+++ generators/app/templates/parcel/app.js
@@ -1,7 +1,7 @@
 import { Elm } from "../Main.elm";
-import * as ElmDebugger from "elm-debug-transformer";
 
 if (process.env.NODE_ENV === 'development') {
+  const ElmDebugger = require("elm-debug-transformer");
   // Only runs in development and will be stripped from production build.
   // See https://parceljs.org/production.html
   ElmDebugger.register();
