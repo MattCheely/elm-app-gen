cbcd2fe7be089a957e786a69584b9dc46404be5e
Matthew Cheely <matt.cheely@gmail.com>
2019-08-14 20:40:10 -0400

    Try to autodetect formatter support for elm-debug-transformer

--- generators/app/templates/parcel/app.js
+++ generators/app/templates/parcel/app.js
@@ -1,10 +1,34 @@
 import { Elm } from "../Main.elm";
 
 if (process.env.NODE_ENV === 'development') {
-  const ElmDebugger = require("elm-debug-transformer");
   // Only runs in development and will be stripped from production build.
   // See https://parceljs.org/production.html
-  ElmDebugger.register();
+
+  const ElmDebugger = require("elm-debug-transformer");
+  function hasFormatterSupport() {
+    const originalFormatters = window.devtoolsFormatters;
+    let supported = false;
+
+    window.devtoolsFormatters = [
+      {
+        header: function(obj, config) {
+          supported = true;
+          return null;
+        },
+        hasBody: function(obj) {},
+        body: function(obj, config) {},
+      },
+    ];
+    console.log('elm-debug-transformer: checking for formatter support.', {});
+    window.devtoolsFormatters = originalFormatters;
+    return supported;
+  }
+
+  if (hasFormatterSupport()) {
+    ElmDebugger.register();
+  } else {
+    ElmDebugger.register({simple_mode: true});
+  }
 }
 
 Elm.Main.init({
